64c2c9a5954bc8c9ba69f2e5f03114dc79aa989b
Lucas Albuquerque <lucas.albuquerque@ccc.ufcg.edu.br>
2021-11-29 20:40:15 -0300

    update seg tree

--- Data Structures/seg_tree.cpp
+++ Data Structures/seg_tree.cpp
@@ -1,71 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+struct node {
+    long long val;
+    node() {
+        val = 0;
+    }
+    node(long long x) {
+        val = x;
+    }
+    node operator+(const node &o) const {
+        node ans;
+        ans.val = val + o.val;
+        return ans;
+    }
+};
  
-template<typename T>
+template<class i_t, class e_t>
 struct seg_tree{
     int n;
-    vector<T> tree, start;
+    vector<i_t> tree;
 
-    seg_tree(vector<T> start_) : start(start_){
-        n = start_.size();
+    seg_tree(const vector<e_t> &a) {
+        n = a.size();
         tree.resize(n * 4);
-        build(1, 1, n);
+        build(1, 1, n, a);
     }
 
-    seg_tree(int n_) : seg_tree(vector<T>(n_)) {}
-
-    T identity(){
-        return 0;
-    }
-
-    T merge(T a, T b){
-        return a + b;
-    }
-    
-    void build(int id, int l, int r){
-        if (l == r) tree[id] = start[l-1];
-        else{
-            int m = (l+r)>>1;
-            build(id<<1, l, m);
-            build(id<<1|1, m+1, r);
-            tree[id] = merge(tree[id<<1], tree[id<<1|1]);
+    void build(int id, int l, int r, const vector<e_t> &a) {
+        if (l == r) tree[id] = i_t(a[l - 1]);
+        else {
+            int m = (l + r)>>1;
+            build(id<<1, l, m, a);
+            build(id<<1|1, m + 1, r, a);
+            tree[id] = tree[id<<1] + tree[id<<1|1];
         }
     }
 
-    void upd(int id, int l, int r, int pos, T val){
-        if (l == r) tree[id] = val;
+    void update(int id, int l, int r, int pos, e_t val) {
+        if (l == r) tree[id] = i_t(val) + tree[id]; // merge / set
         else{
-            int m = (l+r)>>1;
-            if (pos <= m) upd(id<<1, l, m, pos, val);
-            else upd(id<<1|1, m+1, r, pos, val);
-            tree[id] = merge(tree[id<<1], tree[id<<1|1]);
+            int m = (l + r)>>1;
+            if (pos <= m) update(id<<1, l, m, pos, val);
+            else update(id<<1|1, m + 1, r, pos, val);
+            tree[id] = tree[id<<1] + tree[id<<1|1];
         }
     }
 
-    T qry(int id, int l, int r, int lq, int rq){
-        if (l > rq || r < lq) return identity();
+    i_t query(int id, int l, int r, int lq, int rq) {
+        if (l > rq || r < lq) return i_t();
         else if (lq <= l && r <= rq) return tree[id];
         else{
-            int m = (l+r)>>1;
-            return merge(qry(id<<1, l, m, lq, rq), qry(id<<1|1, m+1, r, lq, rq));
+            int m = (l + r)>>1;
+            return query(id<<1, l, m, lq, rq) + query(id<<1|1, m + 1, r, lq, rq);
         }
     }
-
-    void upd(int pos, T val){upd(1, 1, n, pos, val);}
-    T qry(int l, int r){return qry(1, 1, n, l, r);}
+    void update(int pos, e_t val) { update(1, 1, n, pos, val); }
+    i_t query(int l, int r) { return query(1, 1, n, l, r); }
 };
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int n,q; cin >> n >> q;
+    int n, q; cin >> n >> q;
     vector<long long> a(n);
-    for (int i=0; i<n; i++) cin >> a[i];
-    seg_tree<long long> stree(a);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    seg_tree<node, long long> stree(a);
+
     while(q--){
-        int tp, a, b; cin >> tp >> a >> b;
-        if (tp == 1) stree.upd(a, b);
-        else cout << stree.qry(a, b) << '\n';
+        long long tp, x, y; cin >> tp >> x >> y;
+        if (tp == 0) stree.update(1, 1, n, x + 1, y);
+        else cout << stree.query(1, 1, n, x + 1, y).val << '\n';
     }
 }
