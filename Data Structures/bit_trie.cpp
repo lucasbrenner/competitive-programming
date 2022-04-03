b4dfc651965befa0b215649e568ce23a88fb17bf
Lucas Albuquerque <lucas.albuquerque@ccc.ufcg.edu.br>
2022-04-03 16:24:04 -0300

    Add bit trie

---
+++ Data Structures/bit_trie.cpp
@@ -0,0 +1,61 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define endl '\n'
+
+const int LG = 29;
+struct trie {
+    trie *ch[2];
+
+    trie() {
+        ch[0] = ch[1] = nullptr;
+    }
+
+    void insert(int x, int bit = LG) {
+        if (bit < 0) return;
+
+        int nxt = (x & (1 << bit)) > 0;
+        if (!ch[nxt]) ch[nxt] = new trie();
+        ch[nxt]->insert(x, bit - 1);
+    }
+
+    int xor_min(int x, int bit = LG) {
+        if (bit < 0) return 0;
+
+        int nxt = (x & (1 << bit)) > 0;
+        if (ch[nxt]) return ch[nxt]->xor_min(x, bit - 1);
+        return (1 << bit) + ch[nxt ^ 1]->xor_min(x, bit - 1);
+    }
+
+    int xor_max(int x, int bit = LG) {
+        if (bit < 0) return 0;
+
+        int nxt = (x & (1 << bit)) > 0;
+        if (ch[nxt ^ 1]) return (1 << bit) + ch[nxt ^ 1]->xor_max(x, bit - 1);
+        return ch[nxt]->xor_max(x, bit - 1);
+    }
+
+    void get_all(vector<int> &v, int bit = LG, int x = 0) {
+        if (bit < 0) {
+            v.push_back(x);
+            return;
+        }
+        if (ch[0]) ch[0]->get_all(v, bit - 1, x);
+        if (ch[1]) ch[1]->get_all(v, bit - 1, x | (1 << bit));
+    }
+};
+
+int main(){
+    trie root;
+
+    root.insert(0);
+    root.insert(1);
+    root.insert(2);
+    root.insert(3);
+
+    cout << root.xor_min(3) << endl;
+    cout << root.xor_max(3) << endl;
+
+}
+
+
