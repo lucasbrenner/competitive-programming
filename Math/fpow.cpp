a07db45cc892f967b1a0ecc4a250738092682ca3
Lucas Albuquerque <lucasbrenner121@gmail.com>
2022-12-23 20:16:31 -0300

    update math

--- Math/fpow.cpp
+++ Math/fpow.cpp
@@ -1,19 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const long long MOD = 1000000007;
-long long fpow(long long b, long long e){
-    b %= MOD;
-    long long ans = 1;
+typedef long long ll;
+
+const ll mod = 1e9 + 7;
+ll fpow(ll b, ll e) {
+    b %= mod;
+    ll ans = 1;
     while (e > 0) {
-        if (e & 1ll) ans = ans * b % MOD;
-        b = b * b % MOD;
+        if (e & 1ll) ans = ans * b % mod;
+        b = b * b % mod;
         e >>= 1ll;
     }
     return ans;
 }
 
-int main(){
-    long long b, e; cin >> b >> e;
+int main() {
+    ll b, e; cin >> b >> e;
     cout << fpow(b, e) << endl;
 }
+
--- Math/nCk.cpp
+++ Math/nCk.cpp
@@ -1,23 +1,27 @@
-#include<bits/stdc++.h>
+#include"bits/stdc++.h"
 using namespace std;
 
-const long long N = 2e5 + 1, M = 1e9 + 7;
+typedef long long ll;
+
+const long long N = 2e5 + 1, mod = 1e9 + 7;
 long long fn[N], fd[N], iv[N];
 
-void initnck(){
+
+void initnck() {
     iv[1] = fn[1] = fd[1] = fn[0] = fd[0] = 1;
-    for (long long i=2; i<N; i++){
-        iv[i] = (M - (M/i) * iv[M%i] % M) % M;
-        fn[i] = fn[i-1]*i%M;
-        fd[i] = fd[i-1]*iv[i]%M;
+    for (ll i = 2; i < N; i++){
+        iv[i] = (mod - (mod / i) * iv[mod % i] % mod) % mod;
+        fn[i] = fn[i - 1] * i % mod;
+        fd[i] = fd[i - 1] * iv[i] % mod;
     }
 }
 
-long long nck(long long n, long long k){
-    return fn[n] * fd[n-k] % M * fd[k] % M;
+ll nck(ll n, ll k) {
+    return fn[n] * fd[n - k] % mod * fd[k] % mod;
 }
 
-int main(){
+int main() {
     initnck();
     cout << nck(5, 2) << endl;
 }
+
