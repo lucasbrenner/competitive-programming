befc413638faac712cfe581ee79739dfc4792289
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-03-29 10:13:56 -0300

    add min queue

---
+++ data-structures/min_queue.cpp
@@ -0,0 +1,21 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+template<typename T> struct min_queue {
+    int added = 0, removed = 0;
+    deque<pair<T, int>> q;
+
+    T max() { return q.front().first; }
+
+    void add(T val) {
+        while (!q.empty() && q.back().first > val) q.pop_back();
+        q.push_back({val, added});
+        added++;
+    }
+
+    void pop() {
+        while (!q.empty() && q.front().second == removed) q.pop_front();
+        removed++;
+    }
+};
+
