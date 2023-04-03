21b829dce77241c80d389a92ef594563fcd1ae1a
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-04-03 16:50:28 -0300

    add undirected eulerian path

---
+++ Graph/undirected_eulerian_path.cpp
@@ -0,0 +1,44 @@
+// https://cses.fi/problemset/task/1691/
+#include "bits/stdc++.h"
+using namespace std;
+
+#define endl '\n'
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    int n, m; cin >> n >> m;
+    vector<vector<int>> adj(n + 1);
+    vector<array<int, 2>> e(m);
+    for (int i = 0; i < m; i++) {
+        int u, v; cin >> u >> v;
+        adj[u].push_back(i);
+        adj[v].push_back(i);
+        e[i] = { u, v };
+    }
+    for (int i = 1; i <= n; i++) {
+        if (adj[i].size() % 2 == 1) {
+            cout << "IMPOSSIBLE" << endl;
+            return 0;
+        }
+    }
+    vector<int> ans, vis(m);
+    auto dfs = [&](auto self, int v) -> void {
+        while (adj[v].size() > 0) {
+            int i = adj[v].back();
+            adj[v].pop_back();
+            if (!vis[i]) {
+                vis[i] = 1;
+                self(self, e[i][e[i][0] == v]);
+            }
+        }
+        ans.push_back(v);
+    };
+    dfs(dfs, 1);
+    if (ans.size() != m + 1) cout << "IMPOSSIBLE" << endl;
+    else {
+        for (int x : ans) cout << x << " ";
+        cout << endl;
+    }
+}
+
