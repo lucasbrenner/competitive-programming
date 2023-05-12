173433c8b9d4443e39c41575d4c30354d026b3b4
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-05-12 09:37:23 -0300

    upd lca

--- Graph/lca.cpp
+++ Graph/lca.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 struct lca {
     vector<vector<int>> adj, anc;
-    vector<int> depth;
+    vector<int> dep;
     int n, lg;
 
     lca(int n) : n(n) {
-        lg = 33 - __builtin_clz(n);
+        lg = 32 - __builtin_clz(n);
         adj.resize(n + 1);
         anc = vector<vector<int>>(n + 1, vector<int>(lg));
-        depth.resize(n + 1);
+        dep.resize(n + 1);
     }
 
     void add_edge(int u, int v) {
@@ -18,31 +18,20 @@ struct lca {
         adj[v].push_back(u);
     }
 
-    void dfs(int v, int p, int d) {
-        depth[v] = d;
-        for (int c : adj[v]) {
-            if (c == p) continue;
-            anc[c][0] = v;
-            dfs(c, v, d + 1);
-        }
-    }
-
-    void build(int root) {
-        dfs(root, -1, 1);
-        for (int i = 1; i < lg; i++) {
-            for (int v = 1; v <= n; v++) {
-                if (depth[v] - (1 << i) >= 1) {
-                    anc[v][i] = anc[anc[v][i - 1]][i - 1];
-                }
-            }
+    void dfs(int v, int p = -1, int d = 1) {
+        dep[v] = d;
+        for (int i = 1; i < lg; i++) anc[v][i] = anc[anc[v][i - 1]][i - 1];
+        for (int ch : adj[v]) if (ch != p) {
+            anc[ch][0] = v;
+            dfs(ch, v, d + 1);
         }
     }
 
     int get_lca(int a, int b) {
-        if (depth[a] < depth[b]) swap(a, b);
+        if (dep[a] < dep[b]) swap(a, b);
 
         for (int i = lg - 1; i >= 0; i--) {
-            if (depth[a] - depth[b] >= (1 << i)) a = anc[a][i];
+            if (dep[a] - dep[b] >= (1 << i)) a = anc[a][i];
         }
         if (a == b) return a;
 
@@ -56,7 +45,7 @@ struct lca {
     }
 
     int dist(int a, int b) {
-        return depth[a] + depth[b] - 2 * depth[get_lca(a, b)];
+        return dep[a] + dep[b] - 2 * dep[get_lca(a, b)];
     }
 
     // k-th ancestor of v
@@ -83,7 +72,7 @@ int main() {
         g.add_edge(p, i);
     }
 
-    g.build(0);
+    g.dfs(0);
 
     while(q--) {
         int a, b; cin >> a >> b;
