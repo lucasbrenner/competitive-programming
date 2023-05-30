d6ce76caa5618d95b82209d4dd2ce045729d3db1
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-05-30 15:25:03 -0300

    template

--- template.cpp
+++ template.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 #define endl '\n'
+#define all(x) (x).begin(), (x).end()
 typedef long long ll;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
  
 void solvetask() {
 }
  
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin.tie(0)->sync_with_stdio(0);
     int t = 1;
     cin >> t;
     while(t--) solvetask();
