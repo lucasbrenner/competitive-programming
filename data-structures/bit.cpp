b3816fb119508b844aa70c13cf9fc7459e8a8959
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-06-03 22:53:40 -0300

    fdfkdfk

--- data-structures/bit.cpp
+++ data-structures/bit.cpp
@@ -4,22 +4,15 @@ using namespace std;
 template<typename T> struct bit {
     int n;
     vector<T> a;
-
-    bit(int _n) {
-        a.resize(_n + 1);
-        n = _n;
-    }
-
+    bit(int n) : n(n), a(n) {}
     void add(int pos, T x) {
         for (; pos <= n; pos += (pos & -pos)) a[pos] += x;
     }
-
     T query(int pos) {
         T ans = 0;
         for (; pos > 0; pos -= (pos & -pos)) ans += a[pos];
         return ans;
     }
-
     T query(int l, int r) {
         return query(r) - query(l - 1);
     }
