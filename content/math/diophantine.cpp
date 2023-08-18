6338dda969be695aeaa2ef2070b27980716da8e8
Jackson Alves <jackson.souza@ccc.ufcg.edu.br>
2023-08-18 16:37:14 -0300

    add FWHT

--- content/math/diophantine.cpp
+++ content/math/diophantine.cpp
@@ -1,20 +1,6 @@
-#include "../contest/template.cpp"
-
-int gcd(int a, int b, int& x, int& y) {
-    if (b == 0) {
-        x = 1;
-        y = 0;
-        return a;
-    }
-    int x1, y1;
-    int d = gcd(b, a % b, x1, y1);
-    x = y1;
-    y = x1 - y1 * (a / b);
-    return d;
-}
-
+#include "./ext_gcd.cpp"
 bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
-    g = gcd(abs(a), abs(b), x0, y0);
+    g = ext_gcd(abs(a), abs(b), x0, y0);
     if (c % g) {
         return false;
     }
