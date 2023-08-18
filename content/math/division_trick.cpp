f22b52bef7e4ec8b3935ea5a62569d93c3004455
Jackson Alves <jackson.souza@ccc.ufcg.edu.br>
2023-08-18 19:46:59 -0300

    fft, ntt, division trick

---
+++ content/math/division_trick.cpp
@@ -0,0 +1,10 @@
+#include "../contest/template.cpp"
+
+// Division Trick
+//
+// Gera o conjunto n/i, pra todo i, em O(sqrt(n))
+// copiei do github do tfg50
+void division_trick(ll n){
+    for(int l = 1, r; l <= n; l = r + 1) r = n / (n / l);
+    // n / i has the same value for l <= i <= r
+}
