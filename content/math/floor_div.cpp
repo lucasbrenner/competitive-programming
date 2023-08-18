10059d15a86fd341253eccfc10a9716ed94ce87d
Jackson Alves <jackson.souza@ccc.ufcg.edu.br>
2023-08-18 20:15:54 -0300

    ceil_div e floor_div

---
+++ content/math/floor_div.cpp
@@ -0,0 +1,11 @@
+#include "../contest/template.cpp"
+
+ll floor_div(ll x, ll y) {
+    assert(y != 0);
+    if (y < 0) {
+        y = -y;
+        x = -x;
+    }
+    if (x >= 0) return x / y;
+    return (x + 1) / y - 1;
+}
