7506bfb5f7681ddfae33b0ad26a64b1691f1f55b
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-08-18 22:57:23 -0300

    add scc brindges

---
+++ content/graph/bridges.cpp
@@ -0,0 +1,33 @@
+#include "../contest/template.cpp"
+
+int n; // number of nodes
+vector<vector<int>> adj; // adjacency list of graph
+
+vector<int> tin, low, visited;
+int timer;
+
+void dfs(int v, int p = -1) {
+    visited[v] = true;
+    tin[v] = low[v] = timer++;
+    for (int to : adj[v]) {
+        if (to == p) continue;
+        if (visited[to]) {
+            low[v] = min(low[v], tin[to]);
+        } else {
+            dfs(to, v);
+            low[v] = min(low[v], low[to]);
+            if (low[to] > tin[v]) {
+                // (v, to) eh uma ponte
+            }
+        }
+    }
+}
+
+void find_bridges() {
+    timer = 0;
+    visited.assign(n, false);
+    tin.assign(n, -1);
+    low.assign(n, -1);
+    rep(i, 0, n) if (!visited[i]) dfs(i);
+}
+
