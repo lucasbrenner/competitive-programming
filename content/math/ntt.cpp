5dcc54d76163dd52ff7dce30dfcbf26bcb6433cb
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-08-21 20:40:33 -0300

    fix indentation

--- content/math/ntt.cpp
+++ content/math/ntt.cpp
@@ -4,23 +4,24 @@
 
 typedef vector<ll> vl;
 template<int M> vl convMod(const vl &a, const vl &b) {
-	if (a.empty() || b.empty()) return {};
-	vl res(sz(a) + sz(b) - 1);
-	int B=32-__builtin_clz(sz(res)), n=1<<B, cut=int(sqrt(M));
-	vector<C> L(n), R(n), outs(n), outl(n);
-	rep(i,0,sz(a)) L[i] = C((int)a[i] / cut, (int)a[i] % cut);
-	rep(i,0,sz(b)) R[i] = C((int)b[i] / cut, (int)b[i] % cut);
-	fft(L), fft(R);
-	rep(i,0,n) {
-		int j = -i & (n - 1);
-		outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
-		outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
-	}
-	fft(outl), fft(outs);
-	rep(i,0,sz(res)) {
-		ll av = ll(real(outl[i])+.5), cv = ll(imag(outs[i])+.5);
-		ll bv = ll(imag(outl[i])+.5) + ll(real(outs[i])+.5);
-		res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
-	}
-	return res;
+    if (a.empty() || b.empty()) return {};
+    vl res(sz(a) + sz(b) - 1);
+    int B=32-__builtin_clz(sz(res)), n=1<<B, cut=int(sqrt(M));
+    vector<C> L(n), R(n), outs(n), outl(n);
+    rep(i,0,sz(a)) L[i] = C((int)a[i] / cut, (int)a[i] % cut);
+    rep(i,0,sz(b)) R[i] = C((int)b[i] / cut, (int)b[i] % cut);
+    fft(L), fft(R);
+    rep(i,0,n) {
+        int j = -i & (n - 1);
+        outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
+        outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
+    }
+    fft(outl), fft(outs);
+    rep(i,0,sz(res)) {
+        ll av = ll(real(outl[i])+.5), cv = ll(imag(outs[i])+.5);
+        ll bv = ll(imag(outl[i])+.5) + ll(real(outs[i])+.5);
+        res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
+    }
+    return res;
 }
+
