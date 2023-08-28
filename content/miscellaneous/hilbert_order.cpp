f758e2069083a63322be250b9fff4f625df1cc32
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-08-28 19:44:54 -0300

    smaller hilbert curve

--- content/miscellaneous/hilbert_order.cpp
+++ content/miscellaneous/hilbert_order.cpp
@@ -1,25 +1,27 @@
 #include "../contest/template.cpp"
 
-const int rot_delta[4] = { 3, 0, 0, 1 };
-inline ll hilbert_order(int x, int y, int bit, int rot) {
-    if (bit == 0) return 0;
-    int hbit = 1 << (bit - 1);
-    int seg = (x < hbit) ? ((y < hbit) ? 0 : 3) : ((y < hbit) ? 1 : 2);
-    seg = (seg + rot) & 3;
-    int nx = x & (x ^ hbit), ny = y & (y ^ hbit);
-    int nrot = (rot+ rot_delta[seg]) & 3;
-    ll ssize = 1LL << (2 * bit - 2);
-    ll ans = seg * ssize;
-    ll add = hilbert_order(nx, ny, bit - 1, nrot);
-    ans += (seg == 1 || seg == 2) ? add : (ssize - add - 1);
-    return ans;
+inline ll hilbert_order(int x, int y) {
+    int bit = 20;
+    ll res = 0;
+    for (int n = 1 << bit, s = n >> 1; s; s >>= 1) {
+        int rotate_x = (x & s) ? 1 : 0;
+        int rotate_y = (y & s) ? 1 : 0;
+        res += ((rotate_x) ^ (rotate_y * 3LL)) * s * s;
+        if (rotate_x) continue;
+        if (rotate_y) {
+            x = n - 1 - x;
+            y = n - 1 - y;
+        }
+        swap(x, y);
+    }
+    return res;
 }
 
 struct query {
     int l, r, idx;
     ll ord;
     void init() {
-        ord = hilbert_order(l, r, 21, 0);
+        ord = hilbert_order(l, r);
     }
     bool operator < (const query &other) const {
         return ord < other.ord;
