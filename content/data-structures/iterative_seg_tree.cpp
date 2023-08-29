02563baa1f17dbac0a13d7c15dac1b2da7b96c2b
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-08-28 23:35:58 -0300

    update iterative segment tree

--- content/data-structures/iterative_seg_tree.cpp
+++ content/data-structures/iterative_seg_tree.cpp
@@ -1,6 +1,6 @@
 #include "../contest/template.cpp"
 
-template<class T> struct seg_tree{
+template<class T> struct seg_tree {
     struct node {
         T x = LLONG_MAX;
         const node operator + (const node& o) const {
@@ -9,21 +9,20 @@ template<class T> struct seg_tree{
             return ans;
         }
     };
-
     int n;
     vector<node> tree;
     seg_tree(int n) : n(n), tree(n * 2) {}
-    void update(ll i, T f){
+    void update(int i, int f) {
         i += n;
         tree[i].x = f;
-        for(i >>= 1; i >= 1; i >>= 1) tree[i] = tree[i * 2] + tree[i * 2 + 1];
+        for (i >>= 1; i >= 1; i >>= 1) tree[i] = tree[i * 2] + tree[i * 2 + 1];
     }
-    T query(ll a, ll b){
+    T query(int a, int b) {
         node esq;
         node dir;
-        for(a += n, b += n; a <= b; a >>= 1, b >>= 1){
-            if(a % 2) esq = esq + tree[a++];
-            if(b % 2 == 0) dir = tree[b--] + dir;
+        for (a += n, b += n; a <= b; a >>= 1, b >>= 1) {
+            if (a % 2) esq = esq + tree[a++];
+            if (b % 2 == 0) dir = tree[b--] + dir;
         }
         return (esq + dir).x;
     }
