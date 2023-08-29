ca9e1cfffff5f9b1293a71bd67af01caf0310bd8
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-08-28 21:58:20 -0300

    update lazy segment tree

--- content/data-structures/seg_tree_lazy.cpp
+++ content/data-structures/seg_tree_lazy.cpp
@@ -2,31 +2,28 @@
 
 template<class T> struct seg_tree {
     struct node {
-        ll x;
+        T x;
         node() : x(0) {}
-        node(ll x) : x(x) {}
-        node operator + (const node &o) const {
+        node(T x) : x(x) {}
+        node operator+(const node &o) const {
             return node(x + o.x);
         }
     };
     struct lazy_node {
-        ll add, set;
+        T add, set;
         lazy_node() : add(0), set(0) {}
     };
     int n;
     vector<node> tree;
     vector<lazy_node> lazy;
-
     seg_tree(vector<T> a) {
         n = a.size();
         tree.resize(n * 4);
         lazy.resize(n * 4);
         build(1, 0, n - 1, a);
     }
-
     inline int left(int id) { return (id << 1); }
     inline int right(int id) { return (id << 1) | 1; }
-
     void build(int id, int l, int r, const vector<T> &a) {
         if (l == r) tree[id] = node(a[l]);
         else {
@@ -37,7 +34,6 @@ template<class T> struct seg_tree {
         }
     }
     inline void push(int id, int l, int r) {
-        if (!lazy[id].add && !lazy[id].set) return;
         if (lazy[id].set) {
             tree[id].x = (r - l + 1) * lazy[id].set;
             if (l != r) {
@@ -57,17 +53,18 @@ template<class T> struct seg_tree {
             lazy[id].add = 0;
         }
     }
-    void update(int id, int l, int r, int lq, int rq, T val, bool add) {
+    template<bool ADD>
+    void update(int id, int l, int r, int lq, int rq, T val) {
         push(id, l, r);
         if (l > rq || r < lq) return;
         if (lq <= l && r <= rq) {
-            if (add) lazy[id].add += val;
+            if (ADD) lazy[id].add += val;
             else lazy[id].set = val, lazy[id].add = 0;
             push(id, l, r);
         } else {
             int mid = (l + r) >> 1;
-            update(left(id), l, mid, lq, rq, val, add);
-            update(right(id), mid + 1, r, lq, rq, val, add);
+            update<ADD>(left(id), l, mid, lq, rq, val);
+            update<ADD>(right(id), mid + 1, r, lq, rq, val);
             tree[id] = tree[left(id)] + tree[right(id)];
         }
     }
@@ -78,7 +75,8 @@ template<class T> struct seg_tree {
         int mid = (l + r) >> 1;
         return query(left(id), l, mid, lq, rq) + query(right(id), mid + 1, r, lq, rq);
     }
-    void update(int l, int r, T val, bool add) { update(1, 0, n - 1, l, r, val, add); }
+    template<bool ADD>
+    void update(int l, int r, T val) { update<ADD>(1, 0, n - 1, l, r, val); }
     node query(int l, int r) { return query(1, 0, n - 1, l, r); }
 };
 
