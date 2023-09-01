f4e12f0471a500114bb84fe0f52a021093f334b0
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-09-01 20:06:51 -0300

    block cut tree

--- content/math/FWHT.cpp
+++ content/math/FWHT.cpp
@@ -9,13 +9,14 @@
 // O(n log(n))
 
 template<char op, class T> vector<T> FWHT(vector<T> f, bool inv = false) {
-	int n = f.size();
-	for (int k = 0; (n-1)>>k; k++) for (int i = 0; i < n; i++) if (i>>k&1) {
-		int j = i^(1<<k);
-		if (op == '^') f[j] += f[i], f[i] = f[j] - 2*f[i];
-		if (op == '|') f[i] += (inv ? -1 : 1) * f[j];
-		if (op == '&') f[j] += (inv ? -1 : 1) * f[i];
-	}
-	if (op == '^' and inv) for (auto& i : f) i /= n;
-	return f;
+    int n = f.size();
+    for (int k = 0; (n-1)>>k; k++) for (int i = 0; i < n; i++) if (i>>k&1) {
+        int j = i^(1<<k);
+        if (op == '^') f[j] += f[i], f[i] = f[j] - 2*f[i];
+        if (op == '|') f[i] += (inv ? -1 : 1) * f[j];
+        if (op == '&') f[j] += (inv ? -1 : 1) * f[i];
+    }
+    if (op == '^' and inv) for (auto& i : f) i /= n;
+    return f;
 }
+
