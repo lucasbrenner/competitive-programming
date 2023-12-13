bdedf37fac5b0eded2a42d7acfd58ec1b7096ebc
Jackson Souza <souzajack15@gmail.com>
2023-12-13 16:43:56 -0300

    speed lca

--- content/graph/lca.cpp
+++ content/graph/lca.cpp
@@ -2,9 +2,9 @@
 
 struct lca {
     vector<vector<int>> adj, anc;
-    vector<int> dep;
-    int n, lg;
-    lca(int n) : n(n), adj(n + 1), dep(n + 1) {
+    vector<int> dep, in, out;
+    int n, lg, t;
+    lca(int n) : n(n), adj(n + 1), dep(n + 1), in(n + 1), out(n + 1) {
         lg = 32 - __builtin_clz(n);
         anc = vector<vector<int>>(n + 1, vector<int>(lg));
     }
@@ -14,22 +14,23 @@ struct lca {
     }
     void dfs(int v, int p = -1, int d = 1) {
         dep[v] = d;
+        in[v] = t++;
         for (int i = 1; i < lg; i++) anc[v][i] = anc[anc[v][i - 1]][i - 1];
         for (int ch : adj[v]) if (ch != p) {
             anc[ch][0] = v;
             dfs(ch, v, d + 1);
         }
+        out[v] = t++;
+    }
+    bool is_anc(int a, int b){
+        return (in[a] <= in[b]) && (out[a] >= out[b]);
     }
     int get_lca(int a, int b) {
-        if (dep[a] < dep[b]) swap(a, b);
+        if(is_anc(a, b)) return a;
+        if(is_anc(b, a)) return b;
         for (int i = lg - 1; i >= 0; i--) {
-            if (dep[anc[a][i]] >= dep[b]) a = anc[a][i];
-        }
-        if (a == b) return a;
-        for (int i = lg - 1; i >= 0; i--) {
-            if (anc[a][i] != anc[b][i]){
+            if (!is_anc(anc[a][i], b) /*&& *anc[a][i]*/){ //descomentar se for 1 indexado
                 a = anc[a][i];
-                b = anc[b][i];
             }
         }
         return anc[a][0];
@@ -47,4 +48,3 @@ struct lca {
         return v;
     }
 };
-
