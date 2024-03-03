4a937b3c7a185ebd31f7548aeb8213a44aff5b6b
Lucas Albuquerque <lucasbrenner121@gmail.com>
2024-03-03 18:20:54 -0300

    update string hash

--- content/string/hash.cpp
+++ content/string/hash.cpp
@@ -1,35 +1,28 @@
 #include "../contest/template.cpp"
 
-struct poly_hash {
-    const static int N = 2e6 + 10, K = 2;
-    constexpr static ll base[] = { 307, 317 }, mod[] = { 104000711, 104000717 };
+mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
+const int K = 2, P = uniform_int_distribution<int>(256, (int)1e9)(rng); // l > |sigma|, r < min(mod)
+const ll MOD[] = { (int)1e9 + 7, (int)1e9 + 9 };
+ll ph[1000100][K], IT;
 
-    ll iv[N][K], p[N][K], h[N][K];
-
-    ll inverse(ll a, ll b, ll s0 = 1, ll s1 = 0) {
-        return b == 0 ? s0 : inverse(b, a % b, s1, s0 - s1 * (a / b));
-    }
-
-    poly_hash(const string &s) {
-        for (int k = 0; k < K; k++) {
-            p[0][k] = iv[0][k] = 1;
-            p[1][k] = base[k];
-            iv[1][k] = inverse(base[k], mod[k]);
-            for (int i = 2; i < N; i++){
-                p[i][k] = p[i - 1][k] * p[1][k] % mod[k];
-                iv[i][k] = iv[i - 1][k] * iv[1][k] % mod[k];
-            }
+struct hsh {
+    vector<array<ll, K>> h;
+    hsh() {}
+    hsh(const string &s) {
+        h.resize(sz(s));
+        while (IT < sz(s)) {
+            rep(j, 0, K) ph[IT][j] = IT ? ph[IT - 1][j] * P % MOD[j] : 1;
+            IT++;
         }
-        for (int i = 1; i <= (int)s.size(); i++) for (int k = 0; k < K; k++) {
-            h[i][k] = (h[i - 1][k] + (s[i - 1] - 'a' + 1) * p[i][k] % mod[k]) % mod[k];
+        rep(i, 0, sz(s)) rep(j, 0, K) {
+            h[i][j] = ((i > 0 ? h[i - 1][j] * P : 0) + s[i]) % MOD[j];
         }
     }
-
-    array<ll, K> query(int l, int r){
-        l++; r++;
-        array<ll, 2> ans;
-        for (int k = 0; k < K; k++){
-            ans[k] = (h[r][k] - h[l - 1][k] + mod[k]) % mod[k] * iv[l][k] % mod[k];
+    array<ll, K> operator()(int l, int r) {
+        array<ll, K> ans;
+        rep(j, 0, K) {
+            ans[j] = h[r][j] - (l > 0 ? h[l - 1][j] * ph[r - l + 1][j] % MOD[j] : 0);
+            if (ans[j] < 0) ans[j] += MOD[j];
         }
         return ans;
     }
