54aaeca7ac93eff19f9f58801598ff6fdd718060
Lucas Albuquerque <lucasbrenner121@gmail.com>
2024-03-03 18:54:37 -0300

    update string hash

--- content/string/hash.cpp
+++ content/string/hash.cpp
@@ -1,27 +1,24 @@
 #include "../contest/template.cpp"
 
 mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
-const int K = 2, P = uniform_int_distribution<int>(256, (int)1e9)(rng); // l > |sigma|, r < min(mod)
+const int NN = 2e5 + 1, K = 2, P = uniform_int_distribution<int>(256, 1e9)(rng); // l > |sigma|, r < min(mod)
 const ll MOD[] = { (int)1e9 + 7, (int)1e9 + 9 };
-ll ph[1000100][K], IT;
+ll ph[NN][K], hh[NN][K], IT; // LEMBRAR DE CHAMAR pre()!!!!!!!!!!
+void pre() { rep(i, 0, NN) rep(j, 0, K) ph[i][j] = i ? ph[i - 1][j] * P % MOD[j] : 1; }
 
 struct hsh {
-    vector<array<ll, K>> h;
+    int L;
     hsh() {}
     hsh(const string &s) {
-        h.resize(sz(s));
-        while (IT < sz(s)) {
-            rep(j, 0, K) ph[IT][j] = IT ? ph[IT - 1][j] * P % MOD[j] : 1;
-            IT++;
-        }
+        L = IT, IT += sz(s);
         rep(i, 0, sz(s)) rep(j, 0, K) {
-            h[i][j] = ((i > 0 ? h[i - 1][j] * P : 0) + s[i]) % MOD[j];
+            hh[L + i][j] = ((i > 0 ? hh[L + i - 1][j] * P : 0) + s[i]) % MOD[j];
         }
     }
     array<ll, K> operator()(int l, int r) {
         array<ll, K> ans;
         rep(j, 0, K) {
-            ans[j] = h[r][j] - (l > 0 ? h[l - 1][j] * ph[r - l + 1][j] % MOD[j] : 0);
+            ans[j] = hh[L + r][j] - (l > 0 ? hh[L + l - 1][j] * ph[r - l + 1][j] % MOD[j] : 0);
             if (ans[j] < 0) ans[j] += MOD[j];
         }
         return ans;
