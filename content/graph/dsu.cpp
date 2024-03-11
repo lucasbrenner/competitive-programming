a7403fde3066c6054c22a6914c079a88b1fd8d4e
Lucas Albuquerque <lucasbrenner121@gmail.com>
2024-03-11 18:05:55 -0300

    fix dsu

--- content/graph/dsu.cpp
+++ content/graph/dsu.cpp
@@ -1,11 +1,8 @@
 #include "../contest/template.cpp"
 
 struct dsu {
-    int n;
     vector<int> p, rnk;
-    dsu(int n) : n(n), p(n + 1), rnk(n + 1, 1) {
-        iota(p.begin(), p.end(), 0);
-    }
+    dsu(int n) : p(n), rnk(n, 1) { iota(all(p), 0); }
     int find(int x) { return p[x] == x ? x : p[x] = find(p[x]); }
     bool join(int a, int b) {
         a = find(a); b = find(b);
@@ -17,3 +14,4 @@ struct dsu {
     }
     bool same(int a, int b) { return find(a) == find(b); }
 };
+
