2a57b800b2cebbf19e05bcb6f45d3cfcd7aa6a6b
Lucas Albuquerque <lucasbrenner121@gmail.com>
2024-06-21 13:22:00 -0300

    add conv fwht

--- content/math/FWHT.cpp
+++ content/math/FWHT.cpp
@@ -9,14 +9,21 @@
 // O(n log(n))
 
 template<char op, class T> vector<T> FWHT(vector<T> f, bool inv = false) {
-	int n = f.size();
-	for (int k = 0; (n-1)>>k; k++) for (int i = 0; i < n; i++) if (i>>k&1) {
-		int j = i^(1<<k);
-		if (op == '^') f[j] += f[i], f[i] = f[j] - f[i] * 2;
-		if (op == '|') f[i] += f[j] * (inv ? -1 : 1);
-		if (op == '&') f[j] += f[i] * (inv ? -1 : 1);
-	}
-	if (op == '^' and inv) for (auto& i : f) i /= n;
-	return f;
+    int n = f.size();
+    for (int k = 0; (n-1)>>k; k++) for (int i = 0; i < n; i++) if (i>>k&1) {
+        int j = i^(1<<k);
+        if (op == '^') f[j] += f[i], f[i] = f[j] - f[i] * 2;
+        if (op == '|') f[i] += f[j] * (inv ? -1 : 1);
+        if (op == '&') f[j] += f[i] * (inv ? -1 : 1);
+    }
+    if (op == '^' and inv) for (auto& i : f) i /= n;
+    return f;
+}
+
+template<char op> vl conv(vl a, vl b) {
+    a = FWHT<op>(a), b = FWHT<op>(b);
+    rep(i, 0, sz(a)) a[i] *= b[i];
+    a = FWHT<op>(a, 1);
+    return a;
 }
 
