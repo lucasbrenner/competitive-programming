f78b2890304b36f09c62e32c24f3008b3db8d3b1
Lucas Albuquerque <lucasbrenner121@gmail.com>
2024-09-05 17:36:02 -0300

    add rotate matrix

---
+++ content/miscellaneous/rotate_matrix.cpp
@@ -0,0 +1,20 @@
+#include "../contest/template.cpp"
+
+vector<vector<int>> rotate_ccw(vector<vector<int>> a) {
+    int n = sz(a), m = sz(a[0]);
+    vector b(m, vector<int>(n));
+    rep(i, 0, n) rep(j, 0, m) {
+        b[m - j - 1][i] = a[i][j];
+    }
+    return b;
+}
+
+vector<vector<int>> rotate_cw(vector<vector<int>> a) {
+    int n = sz(a), m = sz(a[0]);
+    vector b(m, vector<int>(n));
+    rep(i, 0, n) rep(j, 0, m) {
+        b[j][n - i - 1] = a[i][j];
+    }
+    return b;
+}
+
