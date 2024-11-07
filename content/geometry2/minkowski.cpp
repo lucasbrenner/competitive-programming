f092ceb9aa2413a79cfa95d842a80bd14b7e7ce2
Lucas Albuquerque <lucasbrenner121@gmail.com>
2024-11-06 23:14:54 -0300

    fix minkowski

--- content/geometry2/minkowski.cpp
+++ content/geometry2/minkowski.cpp
@@ -2,7 +2,7 @@
 
 typedef Point<long long> P;
 
-void reorder_polygon(vector<P> & pts){
+void reorder_polygon(vector<P> &pts){
     int pos = 0;
     for(int i = 1; i < pts.size(); i++){
         if(pts[i].y < pts[pos].y || (pts[i].y == pts[pos].y && pts[i].x < pts[pos].x))
@@ -11,25 +11,23 @@ void reorder_polygon(vector<P> & pts){
     rotate(pts.begin(), pts.begin() + pos, pts.end());
 }
 
-vector<P> minkowski(vector<P> A, vector<P> B){
-    // the first vertex must be the lowest
-    reorder_polygon(A);
-    reorder_polygon(B);
-    // we must ensure cyclic indexing
-    A.push_back(A[0]);
-    A.push_back(A[1]);
-    B.push_back(B[0]);
-    B.push_back(B[1]);
-    // main part
+vector<P> minkowski(vector<P> a, vector<P> b){
+    reorder_polygon(a);
+    reorder_polygon(b);
+    a.push_back(a[0]);
+    a.push_back(a[1]);
+    b.push_back(b[0]);
+    b.push_back(b[1]);
     vector<P> result;
     int i = 0, j = 0;
-    while(i < A.size() - 2 || j < B.size() - 2){
-        result.push_back(A[i] + B[j]);
-        auto cross = (A[i + 1] - A[i]).cross(B[j + 1] - B[j]);
-        if(cross >= 0 && i < A.size() - 2)
+    while(i < a.size() - 2 || j < b.size() - 2){
+        result.push_back(a[i] + b[j]);
+        auto cross = (a[i + 1] - a[i]).cross(b[j + 1] - b[j]);
+        if(cross >= 0 && i < a.size() - 2)
             ++i;
-        if(cross <= 0 && j < B.size() - 2)
+        if(cross <= 0 && j < b.size() - 2)
             ++j;
     }
     return result;
 }
+
