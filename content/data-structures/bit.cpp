1cdd20a6bf6bbd029ecce3609035bc0ef0369bb1
Lucas Albuquerque <lucasbrenner121@gmail.com>
2024-12-06 14:00:05 -0300

    add bs to bit

--- content/data-structures/bit.cpp
+++ content/data-structures/bit.cpp
@@ -15,5 +15,23 @@ template<typename T> struct bit {
     T query(int l, int r) {
         return query(r) - query(l - 1);
     }
+    // min R such that a[1] + a[2] + ... + a[R] >= x, -1 if there's no valid R
+    int min_right(T x) {
+        int ans = -1, i = 31 - __builtin_clz(n), ac = 0;
+        for (T cur = 0; i >= 0; i--) if (ac + (1 << i) <= n) {
+            if (cur + a[(1 << i) + ac] >= x) ans = (1 << i) + ac;
+            else ac += (1 << i), cur += a[ac];
+        }
+        return ans;
+    }
+    // max R such that a[1] + a[2] + ... + a[R] <= x. -1 if there's no valid R
+    int max_right(T x) {
+        int i = 31 - __builtin_clz(n), ac = 0;
+        for (T cur = 0; i >= 0; i--) {
+            if (ac + (1 << i) <= n && cur + a[(1 << i) + ac] <= x)
+                ac += (1 << i), cur += a[ac];
+        }
+        return ac - (ac == 0);
+    }
 };
 
